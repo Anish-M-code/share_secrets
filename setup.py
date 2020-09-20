eb495f37d822cd83b9bfca96c7f1d8064285c84e
ANISH M <58029804+Anish-M-code@users.noreply.github.com>
2020-09-20 12:47:37 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@ f=open('README.md')
 des=f.read()
 f.close()
 setup(
-  name = 'share_secrets',         
-  packages = ['share_secrets'],   
+  name = 'share_secret',         
+  packages = ['share_secret'],   
   version = '0.1',    
   license='GPLv3',        
   description = 'share secrets with ease.', 
   long_description=des,
   author = 'Anish M',                  
   author_email = 'aneesh25861@gmail.com',     
-  url = 'https://github.com/anish-m-code/share_secrets',  
-  download_url = 'https://github.com/Anish-M-code/share_secrets/archive/v0.1.tar.gz',    
+  url = 'https://github.com/anish-m-code/share_secret',  
+  download_url = 'https://github.com/Anish-M-code/share_secret/archive/v0.1.tar.gz',    
   keywords = ['Secret sharing', 'cryptography'],  
   classifiers=[
     'Development Status :: 3 - Alpha',      
