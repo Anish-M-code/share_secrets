7dc26b38a8ccde1a95cbdc520bcb4c39a934d16a
ANISH M <58029804+Anish-M-code@users.noreply.github.com>
2020-12-23 07:38:22 +0000

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+setup(
+  name = 'share_secrets',         
+  packages = ['share_secrets'],   
+  version = '0.4',    
+  license='GPLv3',        
+  description = 'share secrets with ease.', 
+  long_description='A simple secret sharing tool done as student project in cryptography.',
+  author = 'Anish M',                  
+  author_email = 'aneesh25861@gmail.com',     
+  url = 'https://github.com/anish-m-code/share_secret',  
+  download_url = 'https://github.com/Anish-M-code/share_secret/archive/v0.3.tar.gz',    
+  keywords = ['Secret sharing', 'cryptography'],  
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development',
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Education',
+    'Topic :: Security :: Cryptography',
+  ],
+)
+
